da6a4d7b1a6bc41a677ec86f548a559da5af829b
Degra <filippodegrandi02@gmail.com>
2023-05-08 15:49:09 +0200

    Add index.js, app.js and .env.template (change into .env locally)

---
+++ index.js
@@ -0,0 +1,15 @@
+const app = require("./app/app.js");
+const moongoose = require("mongoose");
+
+const port = process.env.PORT || 8080;
+
+app.locals.db = mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then ( () => {
+    
+    console.log("Connected to Database");
+    
+    app.listen(port, () => {
+        console.log(`Server listening on port ${port}`);
+    });
+    
+});
