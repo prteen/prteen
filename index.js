12163a138d94c6fb60ea01903678f4796d6a1a8c
topongo <lorenzo.bodini.private@gmail.com>
2023-05-09 19:01:01 +0200

    .env template update for mongodb connection

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const app = require("./app/app.js");
-const mongoose = require("mongoose");
+const app = require("./app/app.js")
+const mongoose = require("mongoose")
+require("dotenv/config")
 
-const port = process.env.PORT || 8080;
+const port = process.env.HTTP_PORT || 8080
+const mongodb_user = process.env.MONGODB_USER
+const mongodb_pass = process.env.MONGODB_PASS
+const mongodb_host = process.env.MONGODB_HOST
+const mongodb_db = process.env.MONGODB_DB
 
-app.locals.db = mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-.then ( () => {
-    
+app.locals.db = mongoose.connect(`mongodb+srv://${encodeURIComponent(mongodb_user)}:${mongodb_pass}@${mongodb_host}/${mongodb_db}`, { ssl: true })
+  .then(() => {  
     console.log("Connected to Database");
-    
-    app.listen(port, () => {
-        console.log(`Server listening on port ${port}`);
-    });
-    
-});
+    // app.listen(port, () => {
+    //     console.log(`Server listening on port ${port}`);
+    // })
+})
+
