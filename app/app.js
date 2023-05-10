c4b99531ef56ba3fd3b1ee2ff25f304c445dace0
topongo <lorenzo.bodini.private@gmail.com>
2023-05-11 01:03:29 +0200

    remove debug lines

--- app/app.js
+++ app/app.js
@@ -4,12 +4,6 @@ const body_parser = require('body-parser')
 const api_router = require("./routes/api")
 const app = express()
 
-const {Crud, CrudSettings} = require("./interfaces/crud")
-
-let x = new Crud(1, 2)
-
-
-
 app.get("/", (req, res) => {
   res.send("Welcome to Prteen! Api endpoint is at <a href=/api/v1/>/api/v1/</a>")
 })
