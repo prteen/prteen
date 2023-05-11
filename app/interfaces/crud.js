d56ce332bb8562d8d782a629bc1c84c8527e7fdc
topongo <lorenzo.bodini.private@gmail.com>
2023-05-11 22:33:40 +0200

    greatly improved crud auto routing

--- app/interfaces/crud.js
+++ app/interfaces/crud.js
@@ -1,16 +1,127 @@
 const express = require("express")
 
 class CrudSettings {
-  static fields = ["identifiers"]
-
   constructor(data) {
-    this.identifiers = null
-    CrudSettings.fields.forEach(field => this[field] = data[field])
+    this.identifiers = data.identifiers || {}
+    this.exclude = data.exclude || []
+  }
+
+  forIdentifiers(callback) {
+    Object.keys(this.identifiers).forEach((key) => {
+      callback(key, this.identifiers[key] || key)
+    })
+  }
+}
+
+const operations = {
+  create(parent, router, route) {
+    console.log(` --> creating operation POST @ ${route}`)
+    router.post("/", async (req, res) => {
+      try {
+        let obj = new parent.model(req.body)
+        await obj.save() 
+        return res.status(200).json({
+          message: "Successfully created new obj",
+          type: "success",
+        })
+      } catch (error) {
+        return res.status(500).json({
+          message: "Failed to create new obj",
+          type: "error",
+          error: error
+        }) 
+      }
+    })
+  },
+  
+  read(parent, router, route) {
+    parent.settings.forIdentifiers((id_db, id_symb) => {
+      console.log(` --> creating operation GET @ ${route}/${id_symb}`)
+      router.get(`/${id_symb}/:id`, (req, res) => {
+        let query = {}
+        query[id_db] = req.params.id
+        parent.model.find(query)
+          .then(obj => res.json(obj))
+          .catch(err => {
+            console.log(err)
+          })
+      })
+    })
+  },
+
+  read_all(parent, router, route) {
+    console.log(` --> creating operation GET @ ${route}/`)
+    router.get("/", (_req, res) => {
+      parent.model.find()
+        .then(objs => {
+          res.json(objs)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    })
+  },
+
+  update(parent, router, route) {
+    parent.settings.forIdentifiers((id_db, id_symb) => {
+      console.log(` --> creating operation PUT @ ${route}/${id_symb}`)
+      router.get(`/${id_symb}/:id`, async (req, res) => {
+        try {
+          let query = {}
+          query[id_db] = req.params.id
+          let obj = await parent.model.findOne(query)
+          obj = Object.assign(obj, req.body)
+          await obj.save()
+          return res.status(200).json({
+            message: "Successfully updated obj",
+            type: "success",
+          })
+        } catch (error) {
+          return res.status(500).json({
+            message: "Failed to update obj",
+            type: "error",
+            error: error
+          }) 
+        }
+      })
+    })
+  },
+
+  delete(parent, router, route) {
+    parent.settings.forIdentifiers((id_db, id_symb) => {
+      console.log(` --> creating operation DELETE @ ${route}/${id_symb}`)
+      router.delete(`/${id_symb}/:id`, async (req, res) => {
+        try {
+          let query = {}
+          query[id_db] = req.params.id
+          let obj = await parent.model.deleteOne(query)
+          if(obj.deletedCount == 0) {
+            return res.status(404).json({
+              message: "Failed to delete obj",
+              type: "error",
+              error: "Object not found"
+            })
+          }
+          return res.status(200).json({
+            message: "Successfully deleted obj",
+            type: "success",
+            response: obj
+          })
+        } catch (error) {
+          throw error
+          return res.status(500).json({
+            message: "Failed to delete obj",
+            type: "error",
+            error: error
+          }) 
+        }
+      })
+    })
   }
 }
 
 
-class Crud {
+class Crud { 
   constructor(model_class, settings) {
     if(typeof settings === "CrudSettings") {
       this.settings = settings
@@ -23,32 +134,13 @@ class Crud {
   register(parent, route) {
     let router = express.Router()
 
-    router.get("/", (req, res) => {
-      this.model.find()
-        .then(objs => {
-          res.json(objs)
-        })
-        .catch(err => {
-          console.log(err)
-        })
+    Object.keys(operations).forEach((operation) => {
+      if(this.settings.exclude.includes(operation)) {
+        return
+      }
+      console.log(`=> Creating operation ${operation} for route ${route}`)
+      operations[operation](this, router, route)
     })
-
-    Object.keys(this.settings.identifiers).forEach((id_name) => {
-      let id_alias = this.settings.identifiers[id_name] || id_name
-
-      console.log(`creating endpoint ${route}/${id_alias}`)
-      router.get(`/${id_alias}/:id`, (req, res) => {
-        let query = {}
-        query[id_name] = req.params.id
-        console.log(query)
-        this.model.find(query)
-          .then(obj => res.json(obj))
-          .catch(err => {
-            console.log(err)
-          })
-      })
-    })
-
 
     parent.use(route, router)
   } 
