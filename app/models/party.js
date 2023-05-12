21de14e156fbe5f8b5b93991e1fafd891cb57233
topongo <lorenzo.bodini.private@gmail.com>
2023-05-12 12:38:07 +0200

    convert subdoc style to reference

--- app/models/party.js
+++ app/models/party.js
@@ -1,17 +1,26 @@
-const {Schema, model} = require('mongoose')
+const {Schema, model, ObjectId} = require('mongoose')
 const {ImageSchema, Image} = require('./image')
-const {UserSchema, User} = require('./user')
+// const {UserSchema, User} = require('./user')
 
 const PartySchema = new Schema({
   title: String,
   description: String,
   tags: [String],
-  image: ImageSchema,
+  image: {
+    type: ObjectId,
+    refer: "Image",
+  },
   date: Date,
   location: String,
-  organizer: UserSchema,
+  organizer: {
+    type: ObjectId,
+    ref: 'User',
+  },
   max_participants: Number,
-  participants: [UserSchema],
+  participants: [{
+    type: ObjectId,
+    ref: 'User'
+  }],
 })
 
 const Party = model('Party', PartySchema)
