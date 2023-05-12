c26dffc0b97f9247ee55f085415d0ab2ae9f7adb
Degra <filippodegrandi02@gmail.com>
2023-05-12 14:58:29 +0200

    Test authorization with protected route

--- app/routes/api.js
+++ app/routes/api.js
@@ -4,10 +4,22 @@ const router = express.Router()
 const parties = require("./parties")
 const users = require("./users")
 const auth = require("./auth")
+const { protected, logged_in } = require("../utils/protected")
 
 parties.register(router, "/parties")
 users.register(router, "/users")
 router.use("/auth", auth)
+
+router.get("/protected", protected, async (req, res) => {
+  try {
+    if (req.user)
+      return res.status(200).json({message: 'User is logged in', type: 'success', user: req.user});
+
+    return res.status(401).json({message: 'User is not logged in', type: 'error'});
+  } catch (err) {
+    return res.status(401).json({message: 'Error in checking login', type: 'error', err}) 
+  }
+})
 
 
 module.exports = router
---
+++ app/utils/protected.js
@@ -0,0 +1,33 @@
+const {verify} = require('jsonwebtoken');
+const {User} = require('../models/user');
+
+// Middleware for protected routes
+// Checks if the user is authenticated and adds the user to the request
+const protected = async (req, res, next) => {
+  const authorization = req.headers["authorization"]
+  if (!authorization) // There's no access tokein in the headers
+    return res.status(401).json({error: 'Unauthorized'});
+
+  // Verify the token 
+  const token = authorization.split(' ')[1];
+  let id;
+  try {
+    id = verify(token, process.env.ACCESS_TOKEN_SECRET).id;
+  } catch (err) {
+    return res.status(500).json({message: 'Invalid token', type: 'error'}); 
+  }
+
+  if (!id) // Invalid tpken 
+    return res.status(401).json({message: 'Invalid token', type: 'error'});
+  
+  // Check if the user exists
+  const user = await User.findById(id);
+  if (!user) // The user doesn't exist
+    return res.status(500).json({message: 'User does not exist', type: 'error'});
+
+  // Add the user to the request
+  req.user = user;
+  next();
+}
+
+module.exports = {protected};
