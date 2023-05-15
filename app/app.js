b26bf64a1c6eecfe10065a95409a9aad1de0d0b1
topongo <lorenzo.bodini.private@gmail.com>
2023-05-15 15:50:27 +0200

    async initialization

--- app/app.js
+++ app/app.js
@@ -2,18 +2,19 @@ const express = require('express')
 const body_parser = require('body-parser')
 const cookie_parser = require('cookie-parser')
 
-const api_router = require("./routes/api")
-const app = express()
+module.exports = async function() {
+  const api_router = require("./routes/api")
+  const app = express()
 
-app.get("/", (_req, res) => {
-  res.send("Welcome to Prteen! Api endpoint is at <a href=/api/v1/>/api/v1/</a>")
-})
+  app.get("/", (_req, res) => {
+    res.send("Welcome to Prteen! Api endpoint is at <a href=/api/v1/>/api/v1/</a>")
+  })
 
-app.use(body_parser.json());
-app.use(body_parser.urlencoded({extended: true}))
-app.use(cookie_parser())
+  app.use(body_parser.json());
+  app.use(body_parser.urlencoded({extended: true}))
+  app.use(cookie_parser())
 
-app.use("/api/v1", api_router)
+  app.use("/api/v1", api_router)
 
-
-module.exports = app
+  return app
+} 
--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-const app = require("./app/app")
+//const app = require("./app/app")
 const {connect} = require("./db/mongodb")
 const {http} = require("./settings")
+const bootstrap = require("./app/app")
 
-app.locals.db = connect()
-  .then(() => {
-    console.log("Connected to Database");
-    app.listen(http.port, () => {
-        console.log(`🚀 Server started on http://127.0.0.1:${http.port}/`);
-    })
+Promise.all([connect(), bootstrap()]).then(() => {
+  console.log(` Server started on http://127.0.0.1:${http.port}/`);
 })
+
+
+// app.locals.db = connect()
+//   .then(() => {
+//     console.log("Connected to Database");
+//     app.listen(http.port, () => {
+//         //     })
+// })
+
 
 
