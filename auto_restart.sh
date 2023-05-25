b690fd34716e144c0e2038cfa9cd3bd44cc16901
topongo <lorenzo.bodini.private@gmail.com>
2023-05-25 17:29:08 +0200

    auto restart app with zsh script

---
+++ auto_restart.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+function update_usage() {
+  USAGE_OLD=$USAGE
+  USAGE=$(du --exclude=.git --exclude=node_modules --bytes --summarize | cut --fields=1)
+}
+
+while true; do
+  update_usage
+  if [[ $USAGE_OLD -ne $USAGE ]]; then
+    if ! [[ -z $PID ]]; then
+      echo "==> Restarting server..."
+      kill -SIGINT $PID
+      wait $PID
+      echo "==> Old server existed."
+    fi
+    npm run dev &
+    PID=$!
+  fi
+  sleep 1
+done
+
+
